1fad38f697c32582552b80d0d1920f3abbe6215b
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-08 09:24:08 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(parameter = 'roller-skate'){
+    return `This Saturday, I want to ${parameter}!`
+}
+saturdayFun("bath my dog")
+function mondayWork(parameter='go to the office'){
+    return `This Monday, I will ${parameter}.`
+}
+mondayWork(' work from home.')
+function wrapAdjective(){
+    
+}
